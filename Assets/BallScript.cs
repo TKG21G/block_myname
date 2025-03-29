2c46c489d1475fd60716df0891b3e061697b4859
TKG21G <104759907+TKG21G@users.noreply.github.com>
2025-03-29 18:18:27 +0900

    環境構築

---
+++ Assets/BallScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+    public float speed = 20.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.GetComponent<Rigidbody>().AddForce(
+            (transform.forward + transform.right) * speed,
+            ForceMode.VelocityChange);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
